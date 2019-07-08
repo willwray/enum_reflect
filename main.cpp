c4e9b9709e9b8fcd77e1a7e21d3136a856ad8c11
Will Wray <wjwray@gmail.com>
2019-07-08 08:26:31 -0400

    Simplify example

--- main.cpp
+++ main.cpp
@@ -1,19 +1,10 @@
-//#include <cstdio>
 #include <iostream>
-
 #include "enumio.hpp"
 
-enum class X { Min32 = INT32_MIN, Min16 = INT16_MIN,
-               Alicia=0, Boole, Bool=Boole, Stott, Doe=0xFF,
-               Max16 = INT16_MAX, Max32 = INT32_MAX };
-int main()
-{
-    using namespace enumio;
+enum class X { Alicia=0, Boole, Bool=Boole, Stott, Doe=0xFF,
+               Min16 = INT16_MIN, Max16 = INT16_MAX,
+               Min32 = INT32_MIN, Max32 = INT32_MAX };
 
-    std::cout << enumerator<X(0)>;
+constexpr auto decl_X = enumio::enum_declaration<X>;
 
-    std::cout << "\n\n" << enumerators<X,id_eq_hex,'\n'> << "\n\n";
-
-    constexpr auto decl = enum_declaration<X>;
-    std::puts( decl );
-}
+int main() { std::puts( decl_X ); }
